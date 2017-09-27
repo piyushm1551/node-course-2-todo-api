75542b3a9d5c08c8385572a6f6be1112a422bbe5
piyush maharana <piyush.maharana@hashtaag.com>
2017-09-27 13:59:41 +0530

    Set up app for heroku

--- server/server.js
+++ server/server.js
@@ -7,6 +7,7 @@ var {mongoose} = require('./db/mongoose');
 var  {Todo} = require('./models/todo');
 var  {Users} = require('./models/user');
 var app = express();
+const port = process.env.PORT || 3000;
 
 app.use(bodyParser.json());
 
@@ -49,8 +50,8 @@ return res.status(404).send('Invalid id');
 });
 
 
-app.listen(3000,() => {
-  console.log('started on port 3000');
+app.listen(port,() => {
+  console.log(`started on port ${port}`);
 });
 
 module.exports = {app};
